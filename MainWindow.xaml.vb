1c4c4a2353ac3050145aa9a252cf76c1d6057891
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-05-25 22:37:03 -0600

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow
+
+    Private ucb = New UltimateChatBot
+
+    Private Sub Label_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
+
+
+        ucb = New UltimateChatBot
+        ucb.show
+
+    End Sub
+End Class
