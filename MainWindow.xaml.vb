96832a9fd0f14fb928de954f3bf2912aa0750836
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-05-26 19:17:30 -0600

    a

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,6 +1,7 @@
 ﻿Class MainWindow
 
     Private ucb = New UltimateChatBot
+    Private p1 = New Page1
 
     Private Sub Label_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
 
@@ -10,12 +11,9 @@
 
     End Sub
 
-    Private Sub MediaElement_MediaEnded(sender As Object, e As RoutedEventArgs)
-        video.Opacity = 0
-    End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        video.Play()
+        f1.Navigate(p1)
     End Sub
 
     Private Sub Border_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
---
+++ Page1.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class Page1
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        video.Pause()
+    End Sub
+
+    Private Sub b2_Click(sender As Object, e As RoutedEventArgs) Handles b2.Click
+        video.LoadedBehavior = MediaState.Play
+        video.Play()
+    End Sub
+End Class
