fb0dc9ce37b276a83b1241bedd0fd170a885401b
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-05-27 00:04:53 -0600

    assas

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -1,7 +1,8 @@
-﻿Class MainWindow
+﻿Imports System.Windows.Media.Animation
+
+Class MainWindow
 
     Private ucb = New UltimateChatBot
-    Private p1 = New Page1
 
     Private Sub Label_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
 
@@ -18,5 +19,32 @@
 
     Private Sub Border_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
 
+    End Sub
+
+    Private Sub video_MediaEnded(sender As Object, e As RoutedEventArgs) Handles video.MediaEnded
+        Transition()
+    End Sub
+
+    Private Sub lblSkip_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles lblSkip.MouseLeftButtonUp
+        Transition()
+    End Sub
+
+    Private Sub Transition()
+        Dim brush As New LinearGradientBrush(Colors.Transparent, Colors.Black, New Point(0, 0), New Point(1, 1))
+        video.OpacityMask = brush
+        Dim animation As New DoubleAnimation(0, TimeSpan.FromSeconds(1))
+        video.BeginAnimation(UIElement.OpacityProperty, animation)
+
+        lblTitulo.Opacity = 100
+        btnIniciar.Opacity = 100
+        btnCerrar.Opacity = 100
+        Icono.Opacity = 100
+
+        lblTitulo.Visibility = Visibility.Visible
+        btnIniciar.Visibility = Visibility.Visible
+        btnCerrar.Visibility = Visibility.Visible
+        Icono.Visibility = Visibility.Visible
+
+        video.Visibility = Visibility.Hidden
     End Sub
 End Class
--- UltimateChatBot.xaml.vb
+++ UltimateChatBot.xaml.vb
@@ -1,3 +1,37 @@
-﻿Public Class UltimateChatBot
+﻿Imports System.Collections.ObjectModel
 
+
+
+Public Class UltimateChatBot
+
+
+    Private items As ObservableCollection(Of String)
+
+    Public Sub New()
+        InitializeComponent()
+        items = New ObservableCollection(Of String)()
+        lsvContenido.ItemsSource = items
+    End Sub
+    Private Sub MediaElement_MediaEnded(sender As Object, e As RoutedEventArgs)
+        backg.Position = TimeSpan.Zero
+        backg.Play()
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        backg.Play()
+    End Sub
+
+    Private Sub Label_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
+
+    End Sub
+
+    Private Sub btnEnviar_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles btnEnviar.MouseLeftButtonUp
+        llenar()
+    End Sub
+
+    Public Sub llenar()
+
+        items.Add(txtInput.Text.ToString())
+        txtInput.Clear()
+    End Sub
 End Class
