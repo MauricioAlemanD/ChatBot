771d3ef832ba241ccec1edebf20cbcf7c62eacc8
Mauricio Leonardo Alemán Díaz <143914334+MauricioAlemanD@users.noreply.github.com>
2024-05-26 18:46:08 -0600

    si

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -9,4 +9,16 @@
         ucb.show
 
     End Sub
+
+    Private Sub MediaElement_MediaEnded(sender As Object, e As RoutedEventArgs)
+        video.Opacity = 0
+    End Sub
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        video.Play()
+    End Sub
+
+    Private Sub Border_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs)
+
+    End Sub
 End Class
